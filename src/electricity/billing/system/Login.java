52ce6baf5962b2b0acb91bee750bd248be16fae1
Harshal1414 <harshalpatel1414@gmail.com>
2023-06-28 11:59:20 +0530

    Initial Commit

---
+++ src/electricity/billing/system/Login.java
@@ -0,0 +1,13 @@
+package electricity.billing.system;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+    Login(){
+        setSize(500, 300);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Login();
+    }
+}
