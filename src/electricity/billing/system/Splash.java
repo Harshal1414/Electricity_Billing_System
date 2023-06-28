56c47d8447b9d5e8ecceb36acf0bd807b3046aef
Harshal1414 <harshalpatel1414@gmail.com>
2023-06-28 10:36:59 +0530

    Icon added

---
+++ src/electricity/billing/system/Splash.java
@@ -0,0 +1,13 @@
+package electricity.billing.system;
+
+import javax.swing.*;
+
+public class Splash extends JFrame {
+    Splash(){
+        setSize(400, 400);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Splash ();
+    }
+}
