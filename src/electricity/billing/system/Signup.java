2b1bc486b5072a272b0fe74dd63ab59bbc448e99
Harshal1414 <harshalpatel1414@gmail.com>
2023-06-30 01:02:12 +0530

    Build Signup page

--- src/electricity/billing/system/Signup.java
+++ src/electricity/billing/system/Signup.java
@@ -1,18 +1,132 @@
 package electricity.billing.system;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class Signup extends JFrame {
+public class Signup extends JFrame implements ActionListener {
+
+    Choice loginAsCho;
+    TextField meterText, employerText, userText, nameText, passText;
+    JButton create, back;
     Signup(){
         super("Signup Page");
+        getContentPane().setBackground(new Color(168, 203, 255));
 
-        JLabel createAs = new JLabel("Create Account As")
+        JLabel createAs = new JLabel("Create Account As");
+        createAs.setBounds(30,50,125,20);
+        add(createAs);
 
-        setSize(600,300);
+        loginAsCho = new Choice();
+        loginAsCho.add("Admin");
+        loginAsCho.add("Customer");
+        loginAsCho.setBounds(170,50,120, 20);
+        add(loginAsCho);
+
+        JLabel meterNo = new JLabel("Meter No.");
+        meterNo.setBounds(30, 100,125,20);
+        meterNo.setVisible(false);
+        add(meterNo);
+
+        meterText = new TextField();
+        meterText.setBounds(170,100,125,20);
+        meterText.setVisible(false);
+        add(meterText);
+
+        JLabel employer = new JLabel("Employer ID");
+        employer.setBounds(30, 100,125,20);
+        employer.setVisible(true);
+        add(employer);
+
+        employerText = new TextField();
+        employerText.setBounds(170,100,125,20);
+        employerText.setVisible(true);
+        add(employerText);
+
+        JLabel userName = new JLabel("Username");
+        userName.setBounds(30,140,125,20);
+        add(userName);
+
+        userText = new TextField();
+        userText.setBounds(170,140,125,20);
+        add(userText);
+
+        JLabel name = new JLabel("Name");
+        name.setBounds(30,180,125,20);
+        add(name);
+
+        nameText = new TextField();
+        nameText.setBounds(170,180,125,20);
+        add(nameText);
+
+        JLabel password = new JLabel("Password");
+        password.setBounds(30,220,125,20);
+        add(password);
+
+        passText = new TextField();
+        passText.setBounds(170,220,125,20);
+        add(passText);
+
+        loginAsCho.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                String user = loginAsCho.getSelectedItem();
+                if(user.equals("Customer")){
+                    employer.setVisible(false);
+                    employerText.setVisible(false);
+                    meterNo.setVisible(true);
+                    meterText.setVisible(true);
+                }
+                else {
+                    employer.setVisible(true);
+                    employerText.setVisible(true);
+                    meterNo.setVisible(false);
+                    meterText.setVisible(false);
+                }
+            }
+        });
+
+        create = new JButton("Create");
+        create.setBackground(new Color(66, 127,219));
+        create.setForeground(Color.black);
+        create.setBounds(50,268,100,25);
+        create.addActionListener(this);
+        add(create);
+
+        back = new JButton("Back");
+        back.setBackground(new Color(66, 127,219));
+        back.setForeground(Color.black);
+        back.setBounds(180,268,100,25);
+        back.addActionListener(this);
+        add(back);
+
+        ImageIcon boyIcon = new ImageIcon(ClassLoader.getSystemResource("Icon/boy.png"));
+        Image boyImg = boyIcon.getImage().getScaledInstance(250,250, Image.SCALE_DEFAULT);
+        ImageIcon boyIcon2 = new ImageIcon(boyImg);
+        JLabel boyLabel = new JLabel(boyIcon2);
+        boyLabel.setBounds(325,30,250,250);
+        add(boyLabel);
+
+        setSize(600,350);
         setLocation(500, 200);
         setLayout(null);
         setVisible(true);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == create){
+
+        }
+        else if (e.getSource() == back) {
+            setVisible(false);
+            new Login();
+        }
+    }
+
     public static void main(String[] args) {
         new Signup();
     }
