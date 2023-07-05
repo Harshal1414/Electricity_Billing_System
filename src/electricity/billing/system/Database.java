d3f45f56a4df1c93ca5f661020e496a7a7b4915f
Harshal1414 <harshalpatel1414@gmail.com>
2023-07-05 20:19:53 +0530

    Adding Driver Class

--- src/electricity/billing/system/Database.java
+++ src/electricity/billing/system/Database.java
@@ -1,18 +1,15 @@
 package electricity.billing.system;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Database {
     Connection connection;
     Statement statement;
-    Database(){
+    Database() {
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Bill_System", "root", "1234");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Bill_system", "root", "1234");
             statement = connection.createStatement();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
