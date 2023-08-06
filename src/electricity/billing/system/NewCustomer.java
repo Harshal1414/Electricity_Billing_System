b546b1a8fd54d79f266d097536dbb0c9e13f1347
Harshal1414 <harshalpatel1414@gmail.com>
2023-08-06 14:35:35 +0530

    Completed NewCustomer file

--- src/electricity/billing/system/NewCustomer.java
+++ src/electricity/billing/system/NewCustomer.java
@@ -2,10 +2,12 @@ package electricity.billing.system;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class NewCustomer extends JFrame {
-    JLabel heading, customerName, meterNum;
-    TextField nameText, meterNumText;
+    JLabel heading, customerName, meterNum, address, city, state, email, phone;
+    JButton next, cancel;
+    TextField nameText, meterNumText, addressText, cityText, stateText, emailText, phoneText;
     NewCustomer(){
         super("New Customer");
 
@@ -31,9 +33,74 @@ public class NewCustomer extends JFrame {
         meterNum.setBounds(50, 120, 100, 20);
         panel.add(meterNum);
 
-        meterNumText = new TextField();
+        meterNumText = new TextField("");
         meterNumText.setBounds(180, 120, 150, 20);
         panel.add(meterNumText);
+
+        address = new JLabel("Address");
+        address.setBounds(50, 160, 100, 20);
+        panel.add(address);
+
+        addressText = new TextField();
+        addressText.setBounds(180, 160, 150, 20);
+        panel.add(addressText);
+
+        city = new JLabel("City");
+        city.setBounds(50, 200, 100, 20);
+        panel.add(city);
+
+        cityText = new TextField();
+        cityText.setBounds(180, 200, 150, 20);
+        panel.add(cityText);
+
+        state = new JLabel("State");
+        state.setBounds(50, 240, 100, 20);
+        panel.add(state);
+
+        stateText = new TextField();
+        stateText.setBounds(180, 240, 150, 20);
+        panel.add(stateText);
+
+        email = new JLabel("Email");
+        email.setBounds(50, 280, 100, 20);
+        panel.add(email);
+
+        emailText = new TextField();
+        emailText.setBounds(180, 280, 150, 20);
+        panel.add(emailText);
+
+        phone = new JLabel("Phone");
+        phone.setBounds(50, 320, 100, 20);
+        panel.add(phone);
+
+        phoneText = new TextField();
+        phoneText.setBounds(180, 320, 150, 20);
+        panel.add(phoneText);
+
+        next = new JButton("Next");
+        next.setBounds(120, 390, 100, 25);
+        next.setBackground(Color.black);
+        next.setForeground(Color.white);
+        panel.add(next);
+
+        cancel = new JButton("Cancel");
+        cancel.setBounds(250, 390, 100, 25);
+        cancel.setBackground(Color.black);
+        cancel.setForeground(Color.white);
+        panel.add(cancel);
+
+        Random ran = new Random();
+        long number = ran.nextLong() % 1000000;
+        meterNumText.setText("" + Math.abs(number));
+
+        setLayout(new BorderLayout());
+        add(panel, "Center");
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/boy.png"));
+        Image i2 = i1.getImage().getScaledInstance(230, 200, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel imageLabel = new JLabel(i3);
+        add(imageLabel, "West");
 
         setSize(700, 500);
         setLocation(400, 200);
