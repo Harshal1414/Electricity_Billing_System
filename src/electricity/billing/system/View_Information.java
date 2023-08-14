28e3b44dca95ecd4826e88f902c126ce5ec8fada
Harshal1414 <harshalpatel1414@gmail.com>
2023-08-14 23:13:29 +0530

    Adding New CLass

---
+++ src/electricity/billing/system/View_Information.java
@@ -0,0 +1,11 @@
+package electricity.billing.system;
+
+public class View_Information {
+    View_Information(){
+
+    }
+
+    public static void main(String[] args) {
+        new View_Information();
+    }
+}
