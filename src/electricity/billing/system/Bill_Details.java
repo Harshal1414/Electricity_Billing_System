e80ee3cc454c68d08d1a7a55b5ecf50c80f343d1
Harshal1414 <harshalpatel1414@gmail.com>
2023-08-15 11:55:20 +0530

    Adding new Class

---
+++ src/electricity/billing/system/Bill_Details.java
@@ -0,0 +1,11 @@
+package electricity.billing.system;
+
+public class Bill_Details {
+    Bill_Details(){
+
+    }
+
+    public static void main(String[] args) {
+        new Bill_Details();
+    }
+}
