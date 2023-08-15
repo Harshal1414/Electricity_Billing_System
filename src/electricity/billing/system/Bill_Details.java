b4033ecba376773a707ff1c264ebf0d59303bacb
Harshal1414 <harshalpatel1414@gmail.com>
2023-08-15 12:07:16 +0530

    Done some changes

--- src/electricity/billing/system/Bill_Details.java
+++ src/electricity/billing/system/Bill_Details.java
@@ -1,11 +1,38 @@
 package electricity.billing.system;
 
-public class Bill_Details {
-    Bill_Details(){
+import net.proteanit.sql.DbUtils;
 
+import javax.swing.*;
+import java.awt.*;
+import java.sql.ResultSet;
+
+public class Bill_Details extends JFrame{
+    String meter;
+    Bill_Details(String meter){
+        this.meter =meter;
+        setSize(700,650);
+        setLocation(400,150);
+        setLayout(null);
+        getContentPane().setBackground(Color.WHITE);
+
+        JTable table = new JTable();
+
+        try{
+            Database c = new Database();
+            String query_bill = "select * from bill where meter_no = '"+meter+"'";
+            ResultSet resultSet = c.statement.executeQuery(query_bill);
+            table.setModel(DbUtils.resultSetToTableModel(resultSet));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        JScrollPane sp = new JScrollPane(table);
+        sp.setBounds(0,0,700,650);
+        add(sp);
+
+        setVisible(true);
     }
 
     public static void main(String[] args) {
-        new Bill_Details();
+        new Bill_Details("");
     }
 }
